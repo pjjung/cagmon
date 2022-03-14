d9134c261ffdf6d6b325a1106ad9753db42f66cf
Phil Jung <35789172+pjjung@users.noreply.github.com>
2022-03-14 09:46:20 +0900

    Update main.py

--- cagmon/main.py
+++ cagmon/main.py
@@ -1,4 +1,17 @@
-#---------------------------------------------------------------------------------------------------------#
+
+# -*- coding: utf-8 -*-
+# Copyright (C) Phil Jung (2020)
+#
+# This file is port of CAGMon.
+#
+# CAGMon is the tool that evaluates the dependence between the primary and auxiliary channels of Gravitational-Wave detectors.
+#
+# CAGMon is following the GNU General Public License version 3. Under this term, you can redistribute and/or modify it.
+# See the GNU free software license for more details.
+
+'''main : main utility to read the GW channel data and the configuration file, and to conduct CAGMon algorithms.
+'''
+
 from cagmon.agrement import *
 import cagmon.melody
 import cagmon.conchord 
