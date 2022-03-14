0fd1d5938c6c9593a8537fbc270d3305f59c2217
Phil Jung <35789172+pjjung@users.noreply.github.com>
2022-03-14 09:51:10 +0900

    Update melody.py

--- cagmon/melody.py
+++ cagmon/melody.py
@@ -1,4 +1,17 @@
-#---------------------------------------------------------------------------------------------------------#
+
+# -*- coding: utf-8 -*-
+# Copyright (C) Phil Jung (2020)
+#
+# This file is port of CAGMon.
+#
+# CAGMon is the tool that evaluates the dependence between the primary and auxiliary channels of Gravitational-Wave detectors.
+#
+# CAGMon is following the GNU General Public License version 3. Under this term, you can redistribute and/or modify it.
+# See the GNU free software license for more details.
+
+'''melody : the core utility for calculating each coefficient.
+'''
+
 from cagmon.agrement import *
 
 __author__ = 'Phil Jung <pjjung@nims.re.kr>'
