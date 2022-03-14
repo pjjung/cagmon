7923f50aaaec0b1ff17cf249dbf26a6affad7bec
Phil Jung <35789172+pjjung@users.noreply.github.com>
2022-03-14 09:52:35 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) Phil Jung (2020)
+#
+# This file is port of CAGMon.
+#
+# CAGMon is the tool that evaluates the dependence between the primary and auxiliary channels of Gravitational-Wave detectors.
+#
+# CAGMon is following the GNU General Public License version 3. Under this term, you can redistribute and/or modify it.
+# See the GNU free software license for more details.
+
 from setuptools import setup, find_packages
 
 with open('README.md', 'r', encoding='utf-8') as fh:
